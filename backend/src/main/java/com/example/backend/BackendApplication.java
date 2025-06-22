9f9d3e10d9bdda74b75bc3c2a2d9ada405493d2f
Rizik Haddad <rihaddad2@uncg.edu>
2025-06-22 01:33:52 -0400

    moved logging logic to main

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("🔍 DB_URL = " + System.getenv("DB_URL"));
+		System.out.println("🔍 DB_USER = " + System.getenv("DB_USER"));
+		System.out.println("🔍 DB_PASSWORD = " + System.getenv("DB_PASSWORD")); // Optional
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
