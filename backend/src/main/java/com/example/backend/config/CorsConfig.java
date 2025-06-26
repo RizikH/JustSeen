8e9921460ca1beca2440072f011f9f0e5458c71f
Rizik Haddad <rihaddad2@uncg.edu>
2025-06-25 20:07:16 -0400

    attemtping to fix cookies on mobile

--- backend/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -14,9 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns(
-                                "http://localhost:3000",
-                                "https://justseen.vercel.app")
+                        .allowedOrigins("https://justseen.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
