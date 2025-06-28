5770f89ae028545c5177233d3d9c01e8a39fc201
Rizik Haddad <rihaddad2@uncg.edu>
2025-06-28 11:20:28 -0400

    added user auth check in root

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
+import { useAuthStore } from "@/components/stores/AuthStore/useAuthStore";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useAuthStore((state) => state.fetchUser());
+  
   return (
     <html lang="en">
       <body
